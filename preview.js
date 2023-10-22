e6640cbaf0c49efe14eee1b20c4776cc465e0f0f
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2023-10-22 22:09:29 +0300

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,8 +1,8 @@
 console.log("preview.js")
+const btn = document.getElementsByClassName('preview');
 function active(){
   const main = document.getElementsByClassName('explorer');
   main.classList.add('active');
   console.log("preview.js: actived")
 }
-const btn = document.getElementsByClassName('preview');
-btn.addEventListener('click', active);
+btn.addEventListener("click", active);
