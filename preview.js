e7feb149dc4a6ad86b8017b7620ed1eddc914acd
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2023-10-22 22:37:21 +0300

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,12 +1,12 @@
-console.log("preview.js 1")
+console.log("preview.js 1");
 const btns = document.getElementsByClassName('preview-btn');
-console.log("preview.js 2")
-for (const btn of btns){
-  console.log("preview.js 3")
-  btn.addEventListener("click", function onClick(){
-    console.log("preview.js 4")
+console.log("preview.js 2");
+for (var i = 0 ; i < comment.length; i++){
+  console.log("preview.js 3");
+  btns[i].addEventListener("click", function onClick(){
+    console.log("preview.js 4");
     const main = document.querySelector('.explorer');
     main.classList.add('active');
-    console.log("preview.js 5")
+    console.log("preview.js 5");
   });
 }
