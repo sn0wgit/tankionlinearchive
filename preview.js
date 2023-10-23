eb20e7b66e8c0c05aaa93454302346f696212761
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2023-10-23 12:24:41 +0300

    Update preview.js

--- preview.js
+++ preview.js
@@ -12,13 +12,26 @@ for (var i = 0 ; i < btns.length; i++){
   });
 }*/
 //ChatGPT
-const btns = document.getElementsByClassName('preview-btn');
+/*const btns = document.getElementsByClassName('preview-btn');
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", (function(index) {
     return function onClick() {
       const main = document.querySelector('.explorer');
       main.classList.add('active');
       console.log("Button " + index + " clicked");
+    };
+  })(i));
+}*/
+//ChatGPT + me
+const btns = document.getElementsByClassName('preview-btn');
+console.log("preview.js 1");
+for (let i = 0; i < btns.length; i++) {
+  console.log("preview.js 1 [" + i + "]");
+  btns[i].addEventListener("click", (function(i) {
+    return function makeActive() {
+      const main = document.querySelector('.explorer');
+      console.log("Button " + i + " clicked");
+      main.classList.add('active');
     };
   })(i));
 }
