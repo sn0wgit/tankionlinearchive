4eaade138c20ea7e69571b9ce924adbe7db0a3e4
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2023-10-23 13:49:16 +0300

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,64 +1,15 @@
-//My
-/*console.log("preview.js 1");
-const btns = document.getElementsByClassName('preview-btn');
-console.log("preview.js 2");
-for (var i = 0 ; i < btns.length; i++){
-  console.log("preview.js 3");
-  btns[i].addEventListener("click", function onClick(){
-    console.log("preview.js 4");
-    const main = document.querySelector('.explorer');
-    main.classList.add('active');
-    console.log("preview.js 5");
-  });
-}*/
-//ChatGPT (1)
-/*const btns = document.getElementsByClassName('preview-btn');
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", (function(index) {
-    return function onClick() {
-      const main = document.querySelector('.explorer');
-      main.classList.add('active');
-      console.log("Button " + index + " clicked");
-    };
-  })(i));
-}*/
-//ChatGPT (1) + me
-/*const btns = document.getElementsByClassName('preview-btn');
-console.log("preview.js 1");
-for (let i = 0; i < btns.length; i++) {
-  console.log("preview.js 1 [" + i + "]");
-  btns[i].addEventListener("click", (function(i) {
-    return function makeActive() {
-      const main = document.querySelector('.explorer');
-      console.log("Button " + i + " clicked");
-      main.classList.add('active');
-    };
-  })(i));
-}*/
-//ChatGPT (2)
-/*const btns = document.getElementsByClassName('preview-btn');
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function onClick() {
-    const main = document.querySelector('.explorer');
-    main.classList.add('active');
-  });
-}*/
-//ChatGPT (2) + me
 function addActive() {
+  console.log("preview.js 4S");
   const main = document.querySelector('.explorer');
   main.classList.add('active');
-  console.log("preview.js 4");
+  console.log("preview.js 4E");
 }
-console.log("preview.js 1");
 const btns = document.getElementsByClassName('preview-btn');
-console.log("preview.js 2");
-console.log(btns.length);
 if (btns.length == 0){
   console.log("preview.js IF");
-  console.log("preview.js 3");
   console.log(btns);
   btns.onclick = function() {
-    console.log("preview.js 3+");
+    console.log("preview.js 3");
     addActive();
   };
   console.log("preview.js 5");
