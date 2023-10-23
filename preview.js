7fff9cc8d1c60aa9e32cdaaec4c95dbb6be121ff
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2023-10-23 13:20:10 +0300

    Update preview.js

--- preview.js
+++ preview.js
@@ -44,6 +44,11 @@ for (let i = 0; i < btns.length; i++) {
   });
 }*/
 //ChatGPT (2) + me
+function addActive() {
+  const main = document.querySelector('.explorer');
+  main.classList.add('active');
+  console.log("preview.js 4");
+}
 console.log("preview.js 1");
 const btns = document.getElementsByClassName('preview-btn');
 console.log("preview.js 2");
@@ -51,22 +56,14 @@ console.log(btns.length);
 if (btns.length == 0){
   console.log("preview.js IF");
   console.log("preview.js 3");
-  btns.addEventListener("click", function onClick() {
-    const main = document.querySelector('.explorer');
-    main.classList.add('active');
-    console.log("preview.js 4");
-  });
+  btns.addEventListener("click", addActive());
   console.log("preview.js 5");
 }
 else{
   console.log("preview.js ELSE");
   for (let i = 0; i < btns.length; i++) {
-    console.log("preview.js 3E [" + i + "]");
-    btns[i].addEventListener("click", function onClick() {
-      const main = document.querySelector('.explorer');
-      main.classList.add('active');
-      console.log("preview.js 4E (" + i + ")");
-    });
-    console.log("preview.js 5E [" + i + "]");
+    console.log("preview.js 3 [" + i + "]");
+    btns[i].addEventListener("click", addActive());
+    console.log("preview.js 5 [" + i + "]");
   }
 }
