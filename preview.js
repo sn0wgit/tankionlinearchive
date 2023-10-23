b61cbfbe0b338961191ec1fe0089c4326308878b
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2023-10-23 14:03:35 +0300

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,27 +1,25 @@
-function addActive() {
-  console.log("preview.js 4S");
-  const main = document.querySelector('.explorer');
-  main.classList.add('active');
-  console.log("preview.js 4E");
-}
-const btns = document.getElementsByClassName('preview-btn');
-if (btns.length == 0){
-  console.log("preview.js IF");
-  console.log(btns);
-  /*btns.onclick = function() {
-    console.log("preview.js 3");
-    addActive();
-  };*/
-  document.querySelector('.preview-btn').addEventListener("click", addActive);
-  console.log("preview.js 5");
-}
-else{
-  console.log("preview.js ELSE");
-//for (let i = 0; i < btns.length; i++) {
-  for (var i in btns) {
-    console.log(btns[i]);
-    console.log("preview.js 3 [" + i + "]");
-    btns[i].addEventListener("mousedown", addActive);
-    console.log("preview.js 5 [" + i + "]");
+document.addEventListener("DOMContentLoaded", function() {
+  function addActive() {
+    console.log("preview.js 4S");
+    const main = document.querySelector('.explorer');
+    main.classList.add('active');
+    console.log("preview.js 4E");
+  }
+  const btns = document.getElementsByClassName('preview-btn');
+  if (btns.length == 0){
+    console.log("preview.js IF");
+    console.log(btns);
+    document.querySelector('.preview-btn').addEventListener("click", addActive);
+    console.log("preview.js 5");
+  }
+  else{
+    console.log("preview.js ELSE");
+  //for (let i = 0; i < btns.length; i++) {
+    for (var i in btns) {
+      console.log(btns[i]);
+      console.log("preview.js 3 [" + i + "]");
+      btns[i].addEventListener("mousedown", addActive);
+      console.log("preview.js 5 [" + i + "]");
+    }
   }
 }
